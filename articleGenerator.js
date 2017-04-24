b8434d331511cffe73d0a3d048fb9d9de311ca2b
Aaron Bowers <aaronjbowers@gmail.com>
2017-04-24 09:32:17 -0600

    Adjusted article generator

--- articleGenerator.js
+++ articleGenerator.js
@@ -1,18 +1,54 @@
 const generateArticle = () => {
   function randomEl(list) {
-    var i = Math.floor(Math.random() * list.length);
+    let i = Math.floor(Math.random() * list.length);
     return list[i];
   }
-  var adjectives = ["adamant", "adroit", "amatory", "animistic", "antic", "arcadian", "baleful", "bellicose", "bilious", "boorish", "calamitous", "caustic", "cerulean", "comely", "concomitant", "contumacious", "corpulent", "crapulous", "defamatory", "didactic", "dilatory", "dowdy", "efficacious", "effulgent", "egregious", "endemic", "equanimous", "execrable", "fastidious", "feckless", "fecund", "friable", "fulsome", "garrulous", "guileless", "gustatory", "heuristic", "histrionic", "hubristic", "incendiary", "insidious", "insolent", "intransigent", "inveterate", "invidious", "irksome", "jejune", "jocular", "judicious", "lachrymose", "limpid", "loquacious", "luminous", "mannered", "mendacious", "meretricious", "minatory", "mordant", "munificent", "nefarious", "noxious", "obtuse", "parsimonious", "pendulous", "pernicious", "pervasive", "petulant", "platitudinous", "precipitate", "propitious", "puckish", "querulous", "quiescent", "rebarbative", "recalcitant", "redolent", "rhadamanthine", "risible", "ruminative", "sagacious", "salubrious", "sartorial", "sclerotic", "serpentine", "spasmodic", "strident", "taciturn", "tenacious", "tremulous", "trenchant", "turbulent", "turgid", "ubiquitous", "uxorious", "verdant", "voluble", "voracious", "wheedling", "withering", "zealous"];
-  var nouns = ["ninja", "chair", "pancake", "statue", "unicorn", "rainbows", "laser", "senor", "bunny", "captain", "nibblet", "cupcake", "carrot", "gnome", "glitter", "potato", "salad", "toejam", "curtain", "beet", "toilet", "exorcism", "stick figure", "mermaid eggs", "sea barnacles", "dragons", "jellybean", "snake", "doll", "bushe", "cookie", "apple", "ice cream", "ukulele", "kazoo", "banjo", "opera singer", "circus", "trampoline", "carousel", "carnival", "locomotive", "hot balloon", "praying manti", "animator", "artisan", "artist", "colorist", "inker", "coppersmith", "director", "designer", "flatter", "stylist", "leadman", "limner", "make-up artist", "model", "musician", "penciller", "producer", "scenographer", "set decorator", "silversmith", "teacher", "auto mechanic", "beader", "bobbin boy", "clerk of the chapel", "filling station attendant", "foreman", "maintenance engineering", "mechanic", "miller", "moldmaker", "panel beater", "patternmaker", "plant operator", "plumber", "sawfiler", "shop foreman", "soaper", "stationary engineer", "wheelwright", "woodworker"];
+  let adjectives = ["adamant", "adroit", "amatory", "animistic", "antic", "arcadian", "baleful", "bellicose", "bilious", "boorish", "calamitous", "caustic", "cerulean", "comely", "concomitant", "contumacious", "corpulent", "crapulous", "defamatory", "didactic", "dilatory", "dowdy", "efficacious", "effulgent", "egregious", "endemic", "equanimous", "execrable", "fastidious", "feckless", "fecund", "friable", "fulsome", "garrulous", "guileless", "gustatory", "heuristic", "histrionic", "hubristic", "incendiary", "insidious", "insolent", "intransigent", "inveterate", "invidious", "irksome", "jejune", "jocular", "judicious", "lachrymose", "limpid", "loquacious", "luminous", "mannered", "mendacious", "meretricious", "minatory", "mordant", "munificent", "nefarious", "noxious", "obtuse", "parsimonious", "pendulous", "pernicious", "pervasive", "petulant", "platitudinous", "precipitate", "propitious", "puckish", "querulous", "quiescent", "rebarbative", "recalcitant", "redolent", "rhadamanthine", "risible", "ruminative", "sagacious", "salubrious", "sartorial", "sclerotic", "serpentine", "spasmodic", "strident", "taciturn", "tenacious", "tremulous", "trenchant", "turbulent", "turgid", "ubiquitous", "uxorious", "verdant", "voluble", "voracious", "wheedling", "withering", "zealous"];
+  let nouns = ["ninja", "chair", "pancake", "statue", "unicorn", "rainbows", "laser", "senor", "bunny", "captain", "nibblet", "cupcake", "carrot", "gnome", "glitter", "potato", "salad", "toejam", "curtain", "beet", "toilet", "exorcism", "stick Figure", "mermaid Egg", "sea Barnacle", "dragon", "jellybean", "snake", "doll", "bushe", "cookie", "apple", "ice Cream", "ukulele", "kazoo", "banjo", "opera Singer", "circus", "trampoline", "carousel", "carnival", "locomotive", "hot balloon", "praying Mantis", "animator", "artisan", "artist", "colorist", "inker", "coppersmith", "director", "designer", "flatter", "stylist", "leadman", "limner", "make-up Artist", "model", "musician", "penciller", "producer", "scenographer", "set Decorator", "silversmith", "teacher", "auto Mechanic", "beader", "bobbin Boy", "clerk of the Chapel", "filling Station Attendant", "foreman", "maintenance Engineering", "mechanic", "miller", "moldmaker", "panel Beater", "patternmaker", "plant Operator", "plumber", "sawfiler", "shop Foreman", "soaper", "stationary Engineer", "wheelwright", "woodworker"];
+
+
 
   let noun = randomEl(nouns)
   let adjective = randomEl(adjectives)
   noun = noun.charAt(0).toUpperCase() + noun.slice(1);
   adjective = adjective.charAt(0).toUpperCase() + adjective.slice(1);
+  let subject = adjective + ' ' + noun
   let headline = "Today the " + adjective + ' ' + noun + " Disappeared!"
   let randomNumber = Math.floor(Math.random() * (800 - 1) + 1)
   let randomGenre = Math.floor(Math.random() * (8 - 1) + 1)
+  let randomHeadline = Math.floor(Math.random() * (10 - 1) + 1)
+
+  switch (randomHeadline) {
+    case 1:
+      headline = 'Today the ' + subject + ' Disappeared!'
+    break;
+    case 2:
+      headline = 'The ' + subject + ', and the Controversy Surrounding the ' + subject + '.'
+    break;
+    case 3:
+      headline = 'The ' + subject + ' Tells All! Rumors Confirmed!'
+    break;
+    case 4:
+      headline = 'New Report! Is the ' + subject + ' a Bad Influence on Your Kids?'
+    break;
+    case 5:
+      headline = 'Breaking: the ' + subject + ' Announces Re-election Bid.'
+    break;
+    case 6:
+      headline = 'The ' + subject + ' Does the Thing!'
+    break;
+    case 7:
+      headline = 'The ' + subject + ' In All Out Twitter War!'
+    break;
+    case 8:
+      headline = 'The Hottest Travel Destinations, as decided by the ' + subject + '.'
+    break;
+    case 9:
+      headline = 'Why I Think the ' + subject + ' is Humanity\'s Best Chance.'
+    break;
+    default:
+
+  }
 
   return {
     img: 'https://unsplash.it/1000/600?image=' + randomNumber,
@@ -21,7 +57,7 @@ const generateArticle = () => {
     postDate: String(new Date()),
     relatedTag: noun,
     genreId: randomGenre,
-    tagline: "Disapearing " + adjective + ' ' + noun + "!",
+    tagline:  adjective + ' ' + noun + "? What could go wrong?",
     views: randomNumber,
     snippet: 'Dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Corem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. <br> <br> Aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Corem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
     body: '<p> <b>Lorem ipsum:</b> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>' +
@@ -37,6 +73,7 @@ const populateArticles = () => {
 
     })
   }
-
 }
+
+
 populateArticles()
